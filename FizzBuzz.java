26c26b1c22aaea713e900a5bc2b48ea2951d6c05
Giles <farringtong@wit.edu>
2018-07-31 10:02:49 -0400

    Mon/Tues practice

---
+++ FizzBuzz.java
@@ -0,0 +1,38 @@
+/*
+ * Scanners
+Wrapper classes
+Strings
+Arrays
+if/else if/else
+Operators
+Params into main method
+Exceptions
+ */
+
+public class FizzBuzz{
+	public static void main(String[] args){
+		/*Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt(); */
+		
+		//access first element of String array
+		String num = args[0];
+		//parse String into int
+		int n = Integer.parseInt(num);
+		
+		for(int i=0; i<n; i++){
+			if(i%15 == 0){
+				System.out.println("FizzBuzz");
+			}
+			else if(i%5 == 0){
+				System.out.println("Buzz");
+			}
+			else if(i%3 == 0){
+				System.out.println("Fizz");
+			}
+			else{
+				System.out.println(i);
+			}
+		
+		}
+	}
+}

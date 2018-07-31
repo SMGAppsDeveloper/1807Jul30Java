42717868715bb29ead1991bba2e9a881899dc5e7
Omar Acevedo Acevedo <omaracevedoacevedo@MacBook-Pro-2.local>
2018-07-31 09:56:03 -0400

    Tuesday morning examples and end of monday

---
+++ Omar_Acevedo_Code/JavaFundamentals/src/com/revature/intro/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.revature.intro;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey you!");
+				
+	}
+
+}
---
+++ Omar_Acevedo_Code/SecondDayProgram/src/com/program/FizzBuzz.java
@@ -0,0 +1,42 @@
+package com.program;
+import java.util.Scanner;
+
+/**
+ * Morning! I’ll be in the room around 9:15. 
+ * Take the first 15 min to write a java program called FizzBuzz
+ * 
+ * It should take in a number n, and print out the numbers 1-n. 
+ * However instead of any multiple of 3, print “Fizz”. 
+ * Any multiple of 5, “Buzz”. And any multiple of both 3 and 5, “FizzBuzz”
+ * You can either take input from a scanner or an argument into your main method
+ * This will be good review.  We’ll go over all concepts involved today
+Put all assignments like this in a folder and push it up to your GitHub
+ * @author omaracevedoacevedo
+ *
+ */
+
+public class FizzBuzz {
+
+//	private static void readFromUser() {
+//		Scanner scanFromUser = new Scanner(System.in);
+//		String s = scanFromUser.next();
+//		int i = scanFromUser.nextInt();
+//		System.out.println(" -> " + i);
+//	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		readFromUser();
+		
+		System.out.println("args[0]");
+		
+		for ( int i = 1; i < 11; i++ ){
+            System.out.println("Count is: " + i);
+		}
+		
+		
+		
+	}
+
+}

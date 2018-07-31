5a276a13b6b18d1ae23a25436c8dfa5c8e0a9eb1
Arthur Panlilio <PanlilioArthur@yahoo.com>
2018-07-31 10:11:29 -0400

    Changed eclipse folder

---
+++ Arthur_Panlilio_Code/Eclipse/Week1Java/javaFundamentals/src/com/revature/intro/HelloWorld.java
@@ -0,0 +1,39 @@
+package com.revature.intro;
+
+import java.util.ArrayList;
+
+/* The first line of any java class is the 
+ * package that is in 
+ * 
+ * Following the package declaration is any
+ * import statements for resources you may
+ * need outisde of this package
+ */
+
+
+//[access modifier][non-access modifiers] class [className] {}
+public class HelloWorld {
+	/*
+	 * public - any class can access this entity
+	 * protected - any class in this package or its subclasses can access
+	 * _(default/package) - only accessible from package
+	 * private - only accessible within the class
+	 * 
+	 */
+	
+	
+	/**
+	 * @author Arthur Panlilio
+	 * @since 7/30/2018
+	 * This is a main function that prints hello world
+	 * 
+	 * @param args
+	 */
+	//[access modifier][non-access modifiers][return type][method name][(parameters)]
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		ArrayList<String> f = new ArrayList<>();
+	}
+
+}
---
+++ Arthur_Panlilio_Code/Eclipse/Week1Java/javaFundamentals/src/com/revature/primitive/Primitives.java
@@ -0,0 +1,22 @@
+package com.revature.primitive;
+
+public class Primitives {
+	/*
+	 * Java has 8 primitive datatypes, which represent raw data
+	 * in an organized form. 
+	 * int, boolean, byte, char, short, double, long
+	 * 
+	 * When a number/string/array/etc is actually written out,
+	 * it is called a literal
+	 */
+	
+	public static void main(String[] args) {
+		int var; //declaring variable;
+		var = 5; //initializing variable
+		int x = 10;
+		x = (int)5.21; //reassigning variable
+		System.out.println(x);
+		char ch = 'a';//Initializing and declaring variable
+	}
+
+}

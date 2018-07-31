3818bcd87ae0b613262e013d83a131e493805799
Mark Bambino <MBambino64@gmail.com>
2018-07-31 09:57:42 -0400

    Added FizzBuzz

---
+++ Mark_Bambino_Code/JavaFundemental/src/com/revature/datatypes/primitives.java
@@ -0,0 +1,26 @@
+package com.revature.datatypes;
+
+public class primitives {
+
+	/*
+	 * int
+	 * boolean
+	 * byte
+	 * float
+	 * double
+	 * char
+	 * short
+	 * long
+	 * 
+	 * static/class scope
+	 * block scope
+	 * method scope
+	 * program scope
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
